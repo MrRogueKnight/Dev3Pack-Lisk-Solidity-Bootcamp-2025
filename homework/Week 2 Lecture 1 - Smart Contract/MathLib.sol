28d08fb53802ac8450b718319add43a17c05423b
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2025-07-23 21:11:15 +0530

    Create MathLib.sol

---
+++ homework/Week 2 Lecture 1 - Smart Contract/MathLib.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library MathLib {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "MathLib: subtraction overflow");
+        return a - b;
+    }
+} 
